8f2b43b89014dae8f40a826751ff877ce0941794
leronewilliams <dayman26@aol.com>
2015-05-11 11:12:57 -0500

    Final R graphs
    Scatterplot Matrix for Hubway data.

--- 03 Visualizations/Scatterplot matrix.R
+++ 03 Visualizations/Scatterplot matrix.R
@@ -1,4 +1,4 @@
-# Scatterplot matrix using "pairs"
+# Scatterplot matrix using "pairs" for Hubway_Stations
 panel.hist <- function(x, ...)
 {
   usr <- par("usr"); on.exit(par(usr))
@@ -11,7 +11,23 @@ panel.hist <- function(x, ...)
 
 pairs(hws[c(2:6)], 
       panel = panel.smooth,  # Optional smoother
-      main = "Scatterplot Matrix for HUbway Data",
+      main = "Scatterplot Matrix for Hubway Data",
       diag.panel = panel.hist, 
       pch = 16, 
       col = "lightgray")
+
+
+# Creating scatterplot matrices
+
+names(hws)
+
+# Basic Scatterplot Matrix
+pairs(~TERMINAL + STATION + MUNICIPAL + LAT + LNG,
+      data = hws, 
+      pch = 20,
+      main = "Simple Scatterplot Matrix for Hubway Stations")
+
+# Use "Pairs Plot" from "psych" package
+install.packages("psych")
+library("psych")
+pairs.panels(hws[c(2, 4, 5, 6)], gap = 0)
--- 03 Visualizations/hook.R
+++
@@ -1,3 +0,0 @@
-# use to knit 3d plots
-require(knitr)
-knit_hooks$set(webgl = hook_webgl)
